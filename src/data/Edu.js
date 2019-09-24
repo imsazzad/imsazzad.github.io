4e217a7c76f5254cfdef8312711d36cb1a986c17
abdul.hasib <abdul.hasib@inflytx.com>
2019-09-24 16:36:32 +0600

    updated edu

--- src/data/Edu.js
+++ src/data/Edu.js
@@ -1,10 +1,26 @@
 export default [
     {
         id: 1,
-        name: "B M S College of Engineering",
-        major: "Computer Science",
-        degree: "Bachelor",
-        dated: "2011 - 2015",
-        cgpa: "9.12"
+        name: "Bangladesh University of Engineering and Technology, Dhaka",
+        major: "Computer Science and Engineering",
+        degree: "Bachelor of Science",
+        dated: "2009 - 2014",
+        cgpa: "3.48"
     },
+    // {
+    //     id: 2,
+    //     name: "Birshreshtha Munshi Abdur Rouf Public College, Dhaka",
+    //     major: "Science",
+    //     degree: "Higher Secondary School Certificate.",
+    //     dated: "2009 - 2014",
+    //     gpa: "5.00"
+    // },
+    // {
+    //     id: 3,
+    //     name: "Motijheel Government Boys’ High School, Dhaka",
+    //     major: "Science",
+    //     degree: "Secondary School Certificate.",
+    //     dated: "1996 - 2006",
+    //     gpa: "5.00"
+    // },
 ];
