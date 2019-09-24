199e0be8a9b36fbe9b3ede1a6dc65a5316efc8d4
abdul.hasib <abdul.hasib@inflytx.com>
2019-09-24 16:36:16 +0600

    changed email

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -3,7 +3,7 @@ import gmail from '../assets/images/gmail.svg'
 export default [
     {
         id: 1,
-        title: "rashmiap.10@gmail.com",
+        title: "abdulhasib@gmail.com",
         icon: gmail,
         linkTo: false,
     },
