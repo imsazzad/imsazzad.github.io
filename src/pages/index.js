1767a891ef9f5d0cc5ce32fb36d16eced7756343
abdul.hasib <abdul.hasib@inflytx.com>
2019-09-24 16:37:58 +0600

    added resume download link

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import RashSelfIcon from '../assets/images/rashmi.svg'
 import AboutBox from '../components/About/AboutBox'
 import about from '../data/About'
-import { UserWrapper, UserTitle, UserDescription, DownloadButton, UserTopic } from '../styles/indexStyle.js'
+import {DownloadButton, UserDescription, UserTitle, UserTopic, UserWrapper} from '../styles/indexStyle.js'
 import Layout from '../components/layout'
 
 const IndexPage = () => {
@@ -14,7 +14,8 @@ const IndexPage = () => {
         <p>
         Hello there, welcome to Md Abdul Hasib's portfolio. He would like to introduce himself as a machine learning engineer(especially deep learning) and backend engineer specialized in python and java. He possesses excellent verbal and written communication skills. He is a quick learner. He has in-depth knowledge of Software Development, Structured and Unstructured Data Analytics, Database Techniques, OS and Data Structures. He can work in a team. He has team-leading experiences also. He is a good motivator, enthusiastic and open to learning new ideas. He has a sound knowledge of recognizing and analyzing information problems. He can handle multiple tasks.
         </p>
-        <DownloadButton href="https://github.com/rashmiap/personal-website-react/raw/master/RashmiAp-Resume.pdf"  download title="Resume">Download Resume</DownloadButton>
+        <DownloadButton href="https://github.com/sazzadBuet08/my_cv_2019/raw/master/MD%20ABDUL%20HASIB.pdf" download
+                        title="Resume">Download Resume</DownloadButton>
       </div>
       <img src={RashSelfIcon} alt="rashmi self" />
     </UserDescription>
@@ -25,6 +26,6 @@ const IndexPage = () => {
     </UserTopic>
   </UserWrapper>
   </Layout>
-}
+};
 
 export default IndexPage
