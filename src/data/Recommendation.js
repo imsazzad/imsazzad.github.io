fc0ed337840395522b0e5de9329a3e49848cc94a
abdul.hasib <abdul.hasib@inflytx.com>
2019-09-24 15:32:34 +0600

    updated recommendation

--- src/data/Recommendation.js
+++ src/data/Recommendation.js
@@ -1,42 +1,50 @@
 export default [
     {
         id: 1,
-        name: "Avinash Saurabh",
-        designation: "Founder and CEO, Zoojoo.BE",
-        userLink: "https://www.linkedin.com/in/avinashsaurabh/",
-        dated: "January 08, 2019",
-        description: "\"Rashmi has been one of our best hires for the engineering team. She joined us as an intern and quickly ramped up to independently manage her tasks. She is fast learner, passionate about her work & keeps herself accountable for timely and high quality deliverables - an asset to any team that she works with.\""
+        name: "Md Sayem Ahmed",
+        designation: "Senior Software Engineer at eBay Kleinanzeigen",
+        userLink: "https://www.linkedin.com/in/sayem64/",
+        dated: "July 13, 2016",
+        description: "\"The very first thing that I can say about Sazzad is that he is a very enthusiastic learner. Back in 2011 we were both part of a Software Engineering study group, and even though he was still a student and had tons of coursework he still managed to attend all the regular study sessions. This also shows that he is a hard worker. He has very good knowledge of OOP and basic Software Engineering as well. As a person he is very humble, modest, and helpful. I wish him all the best in his career!\""
     },
     {
         id: 2,
-        name: "Suvasini Raghavan",
-        designation: "Integrated Marketing Manager, Zoojoo.BE",
-        userLink: "https://www.linkedin.com/in/suvasini-raghavan/",
-        dated: "January 20, 2019",
-        description: "\"I have known Rashmi since the time she joined as an intern. Rashmi worked as a web developer and is extremely quick in understanding and delivering the requirements. Chatty, fun-loving, dedicated, passionate and talented - she was everything that makes for a great colleague!\""
+        name: "Rezwanul Huda",
+        designation: "SOFTWARE CRAFTSMAN | AGILE EVANGELIST | COACH | ARCHITECT",
+        userLink: "https://www.linkedin.com/in/rezwanulhuda/",
+        dated: "August 19, 2016",
+        description: "\"Sazzad and I worked together at Infolytx. He is a young developer with good coding skills and technical knowledge under his belt. He is grooming himself up with a diversified set of software development skills and has a lot of potential. He is very receptive to feedback and takes a great interest in doing things the right way. A very adaptive person Sazzad has the courage to pick up work on areas that he hasn’t worked in before and his enthusiasm is very impressive. I had an enjoyable experience with Sazzad and wish to see him contributing to the community as a great software developer going forward.\""
     },
     {
         id: 3,
-        name: "Snehanjan Shome ",
-        designation: "UX Designer, Aware",
-        userLink: "https://www.linkedin.com/in/snehanjan/",
-        dated: "March 2, 2019",
-        description: "\"Rashmi has been a great asset to the team. Her keen eye for design and quick ability to learn a technology that is best suited for the task at hand meant quick project turn arounds with a phenomenal output. Any team that is dynamic enough to utilize her skills and capabilities would be lucky to have Rashmi as a member. Also, her caring and Dog-loving nature made her a great person to have around in the office. \""
+        name: "Naim Rajiv",
+        designation: "Software Developer at iC Consult",
+        userLink: "https://www.linkedin.com/in/naimrajib/",
+        dated: "June 12, 2016",
+        description: "\"Through the years, we worked on various projects and I was impressed by his way of doing a great job. Sazzad is an achievement oriented co-worker. Brilliant, strong, deadline oriented and precise software engineer, a person you can trust when you want to be sure that important tasks will be completed in a systematic and precise way. I strongly endorse Sazzad. Sazzad is probably one of the best team players to work with I've ever met in my life. \""
     },
     {
         id: 4,
-        name: "Prince Ali Khan",
-        designation: "Software Engineer at RoundGlass",
-        userLink: "https://www.linkedin.com/in/princealikhan/",
-        dated: "January 14, 2018",
-        description: "\"Rashmi is very passionate and has a great vision for her work. I work with Rashmi in Zoojoo.Be for past two years. Her focus keeps everything moving smoothly. She is responsible for web interface of Zoojoo.Be product. She makes sure all the deadline are met and at the same time meets the highest standards. Rashmi spends decent amount of time updating her skill and learn new technologies and implement the same to make the product more efficient and user-friendly.\""
+        name: "Nayeem Ahmad",
+        designation: "CEO, Ishraak",
+        userLink: "https://www.linkedin.com/in/nayeemahmad/",
+        dated: "June 13, 2015",
+        description: "\"Md. Abdul Hasib is an excellent team member. With great programming skills and a continuous learning ability he can entrusted with any challenging assignment.\""
     },
     {
         id: 5,
-        name: "Prashanna Guru",
-        designation: "Growth Product Manager at Great Learning",
-        userLink: "https://www.linkedin.com/in/prashannaguru/",
-        dated: "November 15, 2016",
-        description: "\"Rashmi is very dedicated and gets things done on time. She owns her work and even goes beyond her responsibility to contribute to the product. She also keeps herself updated about new technologies and implements the same to create a fantastic User interface.\""
+        name: "Md Main Uddin Rony",
+        designation: "Ph.D. Student and Graduate Teaching Assistant at University of Maryland",
+        userLink: "https://www.linkedin.com/in/mainuddinrony/",
+        dated: "June 13, 2015",
+        description: "\"He is a great developer with sound technical knowledge. He has expertise in server configuration and Ruby on Rails. We share the same class during our varsity life. He has clear concept on academic topics.\""
+    },
+    {
+        id: 5,
+        name: "M Mahabubur Rhaman",
+        designation: "Software Developer at SPACE SQUIRREL LTD",
+        userLink: "https://www.linkedin.com/in/rony36/",
+        dated: "May 28, 2015",
+        description: "\"Md Abdul Hasib is a proactive and tireless contributor who would make a great addition to any team. He impressed all of us by transforming the ensuing awkward silence into a lively icebreaker discussion among the team members. Throughout the job, He completed his assignments on time and never failed to add a creative touch when appropriate. He has a great knowledge on Ruby, C, C++, Java especially Rails and Server side works like CI server. I would highly recommend him for any position, as he naturally rises to the occasion when presented with something he is passionate about.\""
     }
 ];
