9286191b46034e71833fa8ad62dd0b33105aac11
abdul.hasib <abdul.hasib@inflytx.com>
2019-09-25 12:57:26 +0600

    updated undergrad projects

--- src/data/GradProjects.js
+++ src/data/GradProjects.js
@@ -4,32 +4,99 @@ import quiz from '../assets/images/projects/quiz.svg'
 export default [
     {
         id: 1,
-        title: "ANTIK Web Framework",
+        title: "4 bit Processor design",
         icon: antik,
-        description: "Built a web application for a packet injection toolkit named ANTIK. The tool’s drawback was its non-scalability. The project was to make it deployable on a global platform and to enable users to test the HTTP Apache server from anywhere on the network. The web app enables users to log in securely and trigger the ANTIK tool from a node on the network. Used Django queue to enqueue the HTTP request for processing, which when serviced, triggers the tool with the necessary inputs. The output from the ANTIK tool is fed into a Redis database, where the client can subscribe for that data using Centrifuge. The report when available, is published to the client page. Programmed the front-end using Bootstrap and used Centrifuge, a real-time messaging server, to allow the user to subscribe to the results. Deployed Redis to store the results from the tool and made use of Django-RQ to maintain a queue of workers. Developed by a 3 member team.",
+        description: "Detailed design of a 4 bit processor and its full simulation was done in this project.\n" +
+            "The processor includes 32 instructions, has no pipelines .\n" +
+            "Average CPI is 5 where maximum CPI is 6.\n" +
+            "Simulation was done with Proteus.\n" +
+            "A separate ALU unit and a Modified Booth Multiplier was designed and implemented with LSI chips as part of the project",
         githubPath: null,
         demoPath: null,
-        year: "2014 - 2015",
-        techUsed: "Python, Django, CSS Bootstrap"
+        year: "June 2013",
+        techUsed: null
     },
     {
-      id: 2,
-      title: "Bolly-Quiz : Bollywood quizzing app",
-      description: "A ‘C’ based static application developed using utility tools like RPM, MELD and MAKE. Developed by a 3 member team. This Quiz Application provides 4 categories - Actors, Movies, Musicians and Miscellaneous to pick from and also tracks the highest score and updates it when necessary.",
-      icon: quiz,
-      githubPath: null,
-      demoPath: null,
-      year: "2012",
-      techUsed: "C"
+        id: 3,
+        title: "Doctor’s Community",
+        description: "It is a social networking site like “Facebook”, but mostly for doctors.\n" +
+            "Key features :\n" +
+            "Distinct profile for doctors\n" +
+            "Sharing knowledge between doctors\n" +
+            "Searching doctor’s location with specific criteria\n" +
+            "Specific class of doctor’s community\n" +
+            "Searching and learning on various diseases\n" +
+            "A great chance for advertisement for medicine companies and medical clinics.",
+        icon: quiz,
+        githubPath: null,
+        demoPath: null,
+        year: "June 2012",
+        techUsed: " . NET , C# , oracle 10g"
     },
     {
-      id: 3,
-      title: "Graphical Representation of Sorting Algorithms",
-      description: "Representation of simple sorting algorithms in a graphical way in order to understand the implementation of them with the help of java applets.",
-      icon: quiz,
-      githubPath: null,
-      demoPath: null,
-      year: "2012",
-      techUsed: "Java applets"
+        id: 4,
+        title: "Share Bazar Automation",
+        description: "It is designed and implemented to help a person to buy a company share with some hypothetical analysis and observing the data. It has different indicator giving positive or negative decision by analyzing data of that company’s share.",
+        icon: quiz,
+        githubPath: null,
+        demoPath: null,
+        year: "November 2010",
+        techUsed: "NET , C# , oracle 10g"
+    },
+
+    {
+        id: 5,
+        title: "Media player",
+        description: "My media player can play audio of all format and video of mpeg and mpg format. User can choose which file is to be selected by file chooser for playing",
+        icon: quiz,
+        githubPath: null,
+        demoPath: null,
+        year: "October 2010",
+        techUsed: "Java"
+    },
+    {
+        id: 6,
+        title: "Scientific calculator",
+        description: "It can operate two integer, float of any number of digits. The operations are addition , subtraction , multiplication , division , inverse , logarithm , square , square root , store .",
+        icon: quiz,
+        githubPath: null,
+        demoPath: null,
+        year: "August 2010",
+        techUsed: "Java"
+    },
+    {
+        id: 7,
+        title: "Hearts Game",
+        description: "4 player can play the game . It is a 52 card game . Each player have 13 cards . The player gave the highest card of same suit given by the first player , will be the first player of next deal. A scoreboard was maintained in my game to choose the winner . The player scores lowest point will be the winner.",
+        icon: quiz,
+        githubPath: null,
+        demoPath: null,
+        year: "Feb 2010",
+        techUsed: "C++"
+    },
+    {
+        id: 2,
+        title: "Nachos",
+        description: "Building a thread system\n" +
+            "Implementation of KThread.join()\n" +
+            "Implementation of condition variables directly, by using interrupt enable and disable to provide atomicity.\n" +
+            "Implementation of synchronous send and receive of one word messages (also known as Adastyle rendezvous), using condition variables\n" +
+            "Multiprogramming\n" +
+            "Implementation of the system calls read and write as documented in syscall.h.\n" +
+            "Implementation of support for multiprogramming.\n" +
+            "Implementation of the system calls (exec, join, and exit )\n" +
+            "Caching and Virtual Memory\n" +
+            "Investigation the interaction between the TLB, the virtual memory system, and the file system.\n" +
+            "Implementation softwaremanagement of the TLB, with software translation via an inverted page table.\n" +
+            "Implementation demand paging of virtual memory",
+        icon: quiz,
+        githubPath: null,
+        demoPath: null,
+        year: "October 2012",
+        techUsed: "java"
     }
+
+
 ]
+//"<p>Detailed design of a 4 bit processor and its full simulation was done in this project.<ul> <li>The processor includes 32 instructions, has no pipelines. </li> <li> Average CPI is 5 where maximum CPI is 6.</li> <li>Simulation was done with Proteus. </li> <li>A separate ALU unit and a Modified Booth Multiplier was designed and implemented with LSI chips as part of the project</li></ul></p>",
+// "Detailed design of a 4 bit processor and its full simulation was done in this project.<ul> <li>The processor includes 32 instructions, has no pipelines. </li> <li> Average CPI is 5 where maximum CPI is 6.</li> <li>Simulation was done with Proteus. </li> <li>A separate ALU unit and a Modified Booth Multiplier was designed and implemented with LSI chips as part of the project</li></ui>"
