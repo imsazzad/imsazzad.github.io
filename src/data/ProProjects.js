971160722b4441479a505b3ac30130009bf62ea1
abdul.hasib <abdul.hasib@inflytx.com>
2019-09-26 14:49:50 +0600

    updated professional projects

--- src/data/ProProjects.js
+++ src/data/ProProjects.js
@@ -3,7 +3,6 @@ import like from '../assets/images/projects/social.svg'
 import dare from '../assets/images/projects/dare.svg'
 import journey from '../assets/images/projects/journey.svg'
 import website from '../assets/images/projects/website.svg'
-import aware from '../assets/images/projects/aware.svg'
 import email from '../assets/images/projects/email.svg'
 import vision from '../assets/images/projects/vision.svg'
 import search from '../assets/images/projects/search.svg'
@@ -21,9 +20,19 @@ export default [
             "Then We have to build a multi-tenant software on top of it so that multiple clients can use it. Here\n" +
             "we used YOLO algorithm to identify and localize objects.",
         githubPath: null,
-        demoPath: null,
+        demoPath: "http://deepvision.infolytx.net/",
         year: "July 2018 – Oct 2018",
         techUsed: "Deep Learning, ReactJS, AWS, Serverless Architecture, SQL, JS"
+    },
+    {
+        id: 7,
+        title: "Project Surveillance, Epidemiology, End Results Program (“SEER”)",
+        icon: vision,
+        description: "<p> 'Project SEER' attempts to detect the type (Benign, Uncertain, Carcinoma in Situ, Malignant )of breast cancer based on the SEER dataset from the National Institutes of Health (NIH). To classify the patients, we explored several traditional machine learning and deep learning techniques such as Support vector machine, Decision tree, Logistic regression, Naive Bayes, Feedforward, and Recurrent neural networks. We filled in some gaps in the data by preprocessing using imputation and other techniques. We identified 15 key features (out of 138 attributes) from the dataset, such as “CS Lymph Nodes”, “CS tumor age”, “Age at diagnosis”, “Tumor marker” etc. Our final dataset consisted of ~1.6 million breast cancer records. After training our data model, we achieved ~98% accuracy using a deep learning architecture. Then we tuned the parameters and were able to increase the accuracy to 99.25%. In summary, some of our algorithm’s predictions were accurate 99.25% of the time in detecting which of the 4 classes or types of breast cancer were present in the data.</p>",
+        githubPath: null,
+        demoPath: null,
+        year: "Dec 2017 – Feb 2018",
+        techUsed: "Python, TensorFlow, Keras, Traditional ML and DL"
     },
     {
         id: 8,
@@ -67,27 +76,30 @@ export default [
             "<p>Initial results on test data gave us a macro precision of 87.16% and a macro recall of 82.79%\n" +
             "were very promising. </p>",
         githubPath: null,
-        demoPath: null,
+        demoPath: "https://portal.dbmi.hms.harvard.edu/projects/n2c2-t1/",
         year: "Mar 2018 – May 2018",
         techUsed: "Java, CTakes,"
-    },
-    {
-        id: 7,
-        title: "Project Surveillance, Epidemiology, End Results Program (“SEER”)",
-        icon: vision,
-        description: "<p> 'Project SEER' attempts to detect the type (Benign, Uncertain, Carcinoma in Situ, Malignant )of breast cancer based on the SEER dataset from the National Institutes of Health (NIH). To classify the patients, we explored several traditional machine learning and deep learning techniques such as Support vector machine, Decision tree, Logistic regression, Naive Bayes, Feedforward, and Recurrent neural networks. We filled in some gaps in the data by preprocessing using imputation and other techniques. We identified 15 key features (out of 138 attributes) from the dataset, such as “CS Lymph Nodes”, “CS tumor age”, “Age at diagnosis”, “Tumor marker” etc. Our final dataset consisted of ~1.6 million breast cancer records. After training our data model, we achieved ~98% accuracy using a deep learning architecture. Then we tuned the parameters and were able to increase the accuracy to 99.25%. In summary, some of our algorithm’s predictions were accurate 99.25% of the time in detecting which of the 4 classes or types of breast cancer were present in the data.</p>",
-        githubPath: null,
-        demoPath: null,
-        year: "Dec 2017 – Feb 2018",
-        techUsed: "Python, TensorFlow, Keras, Traditional ML and DL"
     },
     {
         id: 6,
         title: "AnnaLytx: An intelligent conversational virtual agent",
         icon: like,
-        description: "",
+        description: "<p>Anna is our intelligent conversation virtual agent who can help you with all things Infolytx. She can help you learn more about who we are and what we do. She can help you apply for a position with us. She can carry on a conversation with you, joke with you, tell you the weather in Vladivostok, or the time in Tumbuktoo. Here we use Our expertise in NLP, information extraction, AI and Machine Learning coupled with our experience in developing cognitive agents using multiple frameworks including IBM’s Watson, Google’s Dialog flow, and Amazon’s Alexa.</p>" +
+            "<p>Key Features: <p>" +
+            " <ul>" +
+            "<li>Able to participate in the complex and nuanced conversation, where she understands the context</li>" +
+            "<li>Engage in natural sounding small talk to make the conversation appear engaging</li>" +
+            "<li>Support task-directed conversation as she directs prospective candidates to submit applications for potential positions with Infolytx.</li>" +
+            "<li>Answer who and why questions from both local and the wider web content by applying NLP and IR techniques to extract answers to who and why question</li>" +
+            "<li>Demonstrate richer and in-context intelligence by the ability to answer questions about the time and weather globally.</li>" +
+            "</ul></p>" +
+            "<p>What I did here: <p>" +
+            " <ul>" +
+            "<li>Designed the solution so that it can be reused for multiple clients. ( Our team used this framework for another two client projects successfully.)</li>" +
+            "<li>Answered initial who and why questions from conversation & discovery by applying NLP and IR techniques to extract answers to who and why question</li>" +
+            "</ul></p>",
         githubPath: null,
-        demoPath: null,
+        demoPath: "https://www.infolytx.com/our-insights/portfolio-items/annalytx/",
         year: "May 2017 – Dec 2017",
         techUsed: "React, Python, Dialogue-flow, AWS, Watson conversation& discovery etc."
     },
@@ -107,41 +119,99 @@ export default [
         id: 4,
         title: "Random Acts of Pizza : Kaggle Competition",
         icon: journey,
-        description: "Journey feature on the Zoojoobe platform is a set of curated activities that are a set of micro-tasks that a user has to form to achieve a bigger goal like forming a complex habit. Journey feature has multiple levels which changes based on user activity. The whole task list is customised based on user activity.",
+        description: "This competition contains a dataset with 5671 textual requests for pizza from the Reddit community Random Acts of Pizza together with their outcome (successful/unsuccessful) and meta-data. We created an algorithm capable of predicting which requests will garner a cheesy (but sincere!) act of kindness.",
         githubPath: null,
         demoPath: null,
         year: "Nov 2016 – Dec 2016",
-        techUsed: "Python"
+        techUsed: "Python, traditional ML, LDA"
     },
     {
         id: 3,
-        title: "Zoojoobe Pregnancy module",
+        title: "Unstructured Data Analysis",
         icon: pregnancy,
-        description: "Maternity module was primarily designed for the women within a corporate. The feature takes into account the user's due date based on which it recommends articles and relevant mindfulness tracks. This feature helps them stay healthy throughout the process.",
+        description: "<p>Worked with a team to develop a web application, using the Ionic Framework, Java to annotate Risk factors like Hyperlipidemia, Diabetic, Hypertension, Obesity using Regular Expression and other technics and convert it to CDA(Clinical Document Architecture) which is a popular, flexible structure of clinical health records defined by Health Level 7 International (HL7 ) </p>" +
+            "<p>Accomplishments: <p>" +
+            " <ul></li>" +
+            "<li>Designed and developed user-friendly interface for easy navigation</li>" +
+            "<li>Optimized REST responses from a Java-based backend to efficiently send and receive application data</li>" +
+            "<li>Led code management and maintenance for multiple developers using Git, JIRA, TDD, CI/CD, Code Analysis, Code Coverage</li>" +
+            "<li>Worked in a team using Agile methodologies, collaborating with the client and improving the application through continuous feedback.</li>" +
+            "</ul></p>",
         githubPath: null,
-        demoPath: null,
-        year: "2017",
-        techUsed: "React, Material-UI, React-Sound-Manager"
+        demoPath: "http://uda.infolytx.net/",
+        year: "Jun 2016 – Oct 2016",
+        techUsed: "Java, AngularJS, MongoDB, cTakes, NLP CDA, UMLS, SNOMED CT, LOINC etc"
     },
     {
         id: 2,
-        title: "Zoojoobe Sales website",
+        title: "PharmaCoWatch",
         icon: website,
-        description: "Zoojoobe is a game based social wellness platform that unites the workforce to form healthier habits. The platform leverages on the tremendous power of the trusted social network at our workplace to motivate employees to form healthier habits. Developed the sales website on wordpress.",
+        description: "<p>A Java-based PaaS Solution where content from sources like FDA and social media has been parsed, analyzed using Big Data technology to build a Machine Learning Predictive Model for adverse drugs reaction intelligence</p>" +
+            +"<p>Accomplishments:</p>" +
+            " <ul>" +
+            "<li>Optimized REST responses from a Java-based backend to efficiently send and receive application data</li>" +
+            "<li>Led code management and maintenance for multiple developers using Git, JIRA, TDD, CI/CD, Code Analysis, Code Coverage</li>" +
+            "<li>Worked in a team using Agile methodologies, collaborating with the client and improving the application through continuous feedback.</li>" +
+            "<li>Assisted in designing and writing intricate SQL queries to process the data.</li>" +
+            "</ul></p>",
         githubPath: null,
-        demoPath: "https://www.zoojoo.be",
-        year: "2016",
-        techUsed: "HTML, CSS, jquery"
+        demoPath: null,
+        year: "Feb 2016",
+        techUsed: "Hadoop, RapidMiner-ML package, cTakes - Clinical Text Analysis platform, NLP"
     },
     {
         id: 1,
-        title: "Aware Dashboard",
-        icon: aware,
-        description: "Aware is a mindfulness meditation mobile app which provides guided meditation sessions to users. Developed the web front end for Aware Dashboard using ReactJS. Some of the features of this dashboard include User Profile, App related stats, Coupon redemption, Gifting module and Payment gateway integration using Paddle.",
+        title: "Pharmalytx",
+        icon: website,
+        description: "<p>Sales force ennoblement for Pharma</p>" +
+            " <ul>" +
+            "<li>Profiling and Ranking Physicians</li>" +
+            "<li>Generating Drug recommendations for Physicians using Data Analytics (Classification, Clustering, Association rules)</li>" +
+            "</ul></p>",
         githubPath: null,
-        demoPath: "https://my.awaremeditationapp.com/",
-        year: "2017",
-        techUsed: "React, Material-UI, SASS"
+        demoPath: null,
+        year: "Oct 2015",
+        techUsed: "java, aws , rapid miner, xamarin"
     },
+    {
+        id: 10,
+        title: "Bdipo",
+        icon: website,
+        description:
+            " <ul>" +
+            "<li>Automated form filler, result and news of IPO application in Bangladesh, IPO result in Bangladesh</li>" +
+            "<li>Worked on form filler, result of IPO application of bdipo</li>" +
+            "</ul></p>",
+        githubPath: null,
+        demoPath: "http://www.bdipo.com/",
+        year: "Sep 2014 – Aug 2015",
+        techUsed: "java, aws , rapid miner, xamarin"
+    },
+    {
+        id: 11,
+        title: "Stock Portfolio",
+        icon: website,
+        description:
+            " <ul>" +
+            "<li>Shows stocks information of user</li>" +
+            "<li>Used Text parsing to parse user information from text file.</li>" +
+            "<li>Used Ruby on Rails, MySQL, Unit and Integration testing.</li>" +
+            "</ul></p>",
+        githubPath: null,
+        demoPath: "http://www.bdipo.com/",
+        year: "Jun 2015 – Aug 2015",
+        techUsed: "java, aws , rapid miner, xamarin"
+    },
+    {
+        id: 12,
+        title: "RetailAr",
+        icon: website,
+        description: "<p>Retail-Ar is a prototype showcasing the use of 3D modeling and augmented reality to promote specials and offers by retailers wanting to build a social media buzz among their most loyal followers.</p>" +
+            "</p>",
+        githubPath: null,
+        demoPath: "https://www.youtube.com/watch?v=h2QLxQ0xkVo&feature=youtu.be",
+        year: "Jun 2015 – Aug 2015",
+        techUsed: "Ruby on rails, Android, java script, Boot Strap , Nginx, MySql, AWS"
+    }
 
 ];
